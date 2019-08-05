1d6805d84535823719d1a2c88ea9a78e54273003
Siwin Lo <siwin.lo@gmail.com>
2019-08-05 13:14:50 -0400

    bash script

---
+++ bash.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const { pwd } = require("./pwd");
+const { ls } = require("./ls");
+
+//Output a prompt
+process.stdout.write("prompt > ");
+
+//The stdin 'data' event fires after a user types in a line
+process.stdin.on("data", data => {
+  const cmd = data.toString().trim(); //remote the newline
+
+  process.stdout.write("You typed: " + cmd);
+  if (cmd === "pwd") {
+    pwd(cmd);
+  }
+  if (cmd === "ls") {
+    ls(cmd);
+  }
+  process.stdout.write("\nprompt > ");
+});
